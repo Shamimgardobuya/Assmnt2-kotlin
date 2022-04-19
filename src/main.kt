4455bcba5d2ee74cfddc5c5fe8b3ee52f7b6e7ae
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-20 00:10:37 +0300

    assmnt rev

--- src/main.kt
+++ src/main.kt
@@ -5,10 +5,10 @@ fun main(){
     var we = CurrentAccount("23455","Kotlin",560.85)
     we.deposit(145.0)
     println(we.balance)
-    var hair = Product("Braid",34.87,300.0,"other")
-    var fruitzz = Product("Mango",23.89,20.9,"groceries")
-    var ppl = Product("Sub",34.67,400.0,"hygiene")
-    food("Papaya")
+//    var hair = Product("Braid",34.87,300.0,"other")
+//    var fruitzz = Product("Mango",23.89,20.9,"groceries")
+//    var ppl = Product("Sub",34.67,400.0,"hygiene")
+//    food("Papaya")
 
 }
  open class CurrentAccount(var accountnumber:String,var accountname:String,var balance:Double){
@@ -29,17 +29,20 @@ fun main(){
     }
 }
 class Savingsaccount(accountnumber:String, accountname:String, balance:Double):CurrentAccount(accountnumber, accountname, balance){
-   fun withdraw(withdrawals:Int){
-       var withdrawals = 0
-        if (withdrawals<4){
-            withdrawals++
+    override fun withdraw(amount: Double) {
+        super.withdraw(amount)fun withdraw(withdrawals:Int){
+            var withdrawals = 0
+
+            if (withdrawals<4){
+                withdrawals++
+
 
 
 
         }
 
     }
-}
+
 fun food(s:String):String{
     var t = ""
     s.forEachIndexed{index,c->
